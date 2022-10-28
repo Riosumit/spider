fba6d21f1bbec7e85fb6dcfe3d247ec9341186d5
Riosumit <rajsumit22032003@gmail.com>
2022-10-28 21:59:22 +0530

    nothing

--- script.js
+++ script.js
@@ -10,23 +10,18 @@ async function job(){
     let s2="ON";
     let s="";
     let s_on="";
-    for(let j=0; j<color1.length;j++){
-        element1.innerHTML="";
-        element2.innerHTML="";
-        let s="";
-        let s_on="";
-        for(let i=0;i<s1.length;i++){
-            s+=s1[i];
-            element1.innerHTML=s;
-            await sleep(200);
-        }
-        element2.style.color=color1[j];
-        for(let i=0;i<s2.length;i++){
-            s_on+=s2[i];
-            element2.innerHTML=s_on;
-            await sleep(200);
-        }
-        await sleep(1000);
+    element1.innerHTML="";
+    element2.innerHTML="";
+    for(let i=0;i<s1.length;i++){
+        s+=s1[i];
+        element1.innerHTML=s;
+        await sleep(200);
     }
+    for(let i=0;i<s2.length;i++){
+        s_on+=s2[i];
+        element2.innerHTML=s_on;
+        await sleep(200);
+    }
+    await sleep(1000);
     setInterval(job(),2000);
 }
